b6ca32166235caf0c8625d07c048dd35cdf1b13a
takuma1012 <stealth.taku@gmail.com>
2018-06-06 07:21:16 +0000

    クレジット例外処理追加

--- app/controllers/muzzle/orders_controller.rb
+++ app/controllers/muzzle/orders_controller.rb
@@ -23,58 +23,75 @@ class Muzzle::OrdersController < Muzzle::Base
 
   def create
     @orders = History.new(order_params)
-    if @orders.payment == "振込み"
-      @orders.pay_status = "未納"
-      @orders.article_histories.each do |oah|
-        if oah.article.stock < oah.quantity
-          return redirect_to muzzle_orders_new_path, notice: '在庫不足の為、ご注文を承れませんでした。申し訳ございません。'
+
+# 商品が0以下で購入された時の処理
+    if @orders.price.to_i <= 1000
+      return redirect_to muzzle_orders_new_path, notice: "現在の数量では購入できません。\n 商品を１つ以上選択必要があります。"
+    else
+
+# 支払方法別の会計処理
+# 振込みの場合
+      if @orders.payment == "振込み"
+        @orders.pay_status = "未納"
+        @orders.article_histories.each do |oah|
+          if oah.article.stock < oah.quantity
+            return redirect_to muzzle_orders_new_path, notice: '在庫不足の為、ご注文を承れませんでした。申し訳ございません。'
+          end
+          oah.article.update(stock: oah.article.stock - oah.quantity)
         end
-        oah.article.update(stock: oah.article.stock - oah.quantity)
-      end
-      if @orders.save
-         InformationMailer.thanks(@orders).deliver!
-         session[:article_id] = nil
-         @@article_array = []
-         redirect_to muzzle_orders_thank_path
-       else
-         render :new
-      end
-
-    elsif @orders.payment == "クレジット"
-
-      Payjp.api_key = ENV['PAYJP_API_SECRET']
-
-      token =Payjp::Token.create(
-        :card => {
-          :number => params[:number],
-          :cvc => params[:cvc],
-          :exp_month => params[:exp_month],
-          :exp_year => params[:exp_year]})
-
-      charge = Payjp::Charge.create(
-        :amount => params[:amount],
-        :card => token.id,
-        :currency => 'jpy')
-
-      @orders.pay_status = "済"
-      @orders.article_histories.each do |oah|
-        if oah.article.stock < oah.quantity
-          return redirect_to muzzle_orders_new_path, notice: '在庫不足の為、ご注文を承れませんでした。申し訳ございません。'
+        if @orders.save
+           InformationMailer.thanks(@orders).deliver!
+           session[:article_id] = nil
+           @@article_array = []
+           redirect_to muzzle_orders_thank_path
+         else
+           render :new
         end
-        oah.article.update(stock: oah.article.stock - oah.quantity)
-      end
-      if  @orders.save
-          InformationMailer.credit(@orders).deliver!
-          session[:article_id] = nil
-          @@article_array = []
-          redirect_to muzzle_orders_thank_path
-       else
-          render :new
+
+# クレジットの場合
+      elsif @orders.payment == "クレジット"
+        begin
+          Payjp.api_key = ENV['PAYJP_API_SECRET']
+
+          token =Payjp::Token.create(
+            :card => {
+              :number => params[:number],
+              :cvc => params[:cvc],
+              :exp_month => params[:exp_month],
+              :exp_year => params[:exp_year]})
+
+          charge = Payjp::Charge.create(
+            :amount => params[:amount],
+            :card => token.id,
+            :currency => 'jpy')
+
+          @orders.pay_status = "済"
+          @orders.article_histories.each do |oah|
+            if oah.article.stock < oah.quantity
+              return redirect_to muzzle_orders_new_path, notice: '在庫不足の為、ご注文を承れませんでした。申し訳ございません。'
+            end
+            oah.article.update(stock: oah.article.stock - oah.quantity)
+          end
+          if  @orders.save
+              InformationMailer.credit(@orders).deliver!
+              session[:article_id] = nil
+              @@article_array = []
+              redirect_to muzzle_orders_thank_path
+           else
+              render :new
+          end
+
+# クレジットの例外処理
+          rescue Payjp::CardError
+            return redirect_to muzzle_orders_new_path, notice: 'クレジットカードが認証できませんでした。お手数ですが再度、番号をご確認下さい。'
+        end
       end
     end
   end
 
   def destroy
+    @@article_array = []
+    redirect_to muzzle_orders_new_path
   end
 
   def thank
